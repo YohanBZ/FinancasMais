698ce0ce4c2c4f184f4f0fdec560ff1f9340a799
YohanBZ <barbozayohan@gmail.com>
2023-10-04 22:30:36 -0300

    style: :art: Instalação do styled-component

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import LandingPage from './pages/LandingPage.jsx';
+import { GlobalStyle } from './Styles/GlobalStyle.js';
+import { ThemeProvider } from 'styled-components';
+import { ThemeStyle } from './Styles/theme.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <LandingPage />
+    <ThemeProvider theme={ThemeStyle}>
+      <GlobalStyle />
+      <LandingPage />
+    </ThemeProvider>
   </React.StrictMode>,
 );
